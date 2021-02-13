dad2ea6859a36ab720e9c8898a6e610f106b4e7e
Сергей <gan7er@yandex.ru>
2021-02-13 18:09:41 +0300

    Очистка мастер ветки Lesson1/Lesson1/Program.cs

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -10,11 +10,7 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            DateTime data = new DateTime(2021, 02, 11, 19, 07, 45);
-            //DateTime data = DateTime.Now; Вариант с текущей системной датой и временем 
-            string name = "Сергей";
-
-            Console.WriteLine($"Привет,{name},сегодня {data}");
+            
         }
     }
 }
